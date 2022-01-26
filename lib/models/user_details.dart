e466616d4695f69da68d59d63a3828b8e3bbedb2
triipaathii <myself10121990@gmail.com>
2022-01-27 01:05:55 +0530

    user model in progress

---
+++ lib/models/user_details.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/material.dart';
+
+class UserDetail {
+  String? name;
+  String? username;
+  String? userMobile;
+  String? userCountry;
+  String? userState;
+  String? userPinCode;
+  String? userJob;
+  bool? userDeliveryInProgress;
+  bool? userSingleMother;
+  int? userChildNo;
+  int? userInitialScore;
+  List<String>? userPreferredContacts;
+
+}
