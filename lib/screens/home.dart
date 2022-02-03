a23ea80a490e59ec699811e1624787da53550228
triipaathii <myself10121990@gmail.com>
2022-02-03 23:21:46 +0530

    homescreen menu work in progress

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -36,6 +36,7 @@ class HomePage extends StatelessWidget {
                   margin: const EdgeInsets.all(20),
                   child: Column(
                     children: <Widget>[
+
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
@@ -56,16 +57,228 @@ class HomePage extends StatelessWidget {
                         ],
                       ),
 
-                      const SizedBox(height: 20,),
+                      const SizedBox(height: 40,),
 
                       GridView.count(
                         crossAxisCount: 2,
+                        mainAxisSpacing: 15,
+                        crossAxisSpacing: 15,
+                        shrinkWrap: true,
+                        scrollDirection: Axis.vertical,
+                        primary: false,
                         children: <Widget>[
+                          // Postpartum Depression
                           InkWell(
-                            child: Image.asset(
+                            onTap: (){},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(20)
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 15,
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    const SizedBox(),
+                                    Image.asset(
+                                      "assets/images/postpartumDepression.png",
+                                      height: 80,
+                                    ),
+                                    const SizedBox(),
+                                    const Text(
+                                      "About this",
+                                      style: TextStyle(
+                                          fontFamily: "Lemon Milk",
+                                          fontSize: 18,
+                                          color: Colors.black
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ),
+                          //Community
+                          InkWell(
+                              onTap: (){},
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white.withOpacity(0.5),
+                                    borderRadius: BorderRadius.circular(20)
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      const SizedBox(),
+                                      Image.asset(
+                                        "assets/images/community.png",
+                                        height: 80,
+                                      ),
+                                      const SizedBox(),
+                                      const Text(
+                                        "Community",
+                                        style: TextStyle(
+                                            fontFamily: "Lemon Milk",
+                                            fontSize: 18,
+                                            color: Colors.black
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                          ),
+                          //Appointment
+                          InkWell(
+                              onTap: (){},
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white.withOpacity(0.5),
+                                    borderRadius: BorderRadius.circular(20)
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      const SizedBox(),
+                                      Image.asset(
+                                        "assets/images/appointment.png",
+                                        height: 80,
+                                      ),
+                                      const SizedBox(),
+                                      const Text(
+                                        "Appointment",
+                                        style: TextStyle(
+                                            fontFamily: "Lemon Milk",
+                                            fontSize: 18,
+                                            color: Colors.black
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                          ),
+                          //Diary
+                          InkWell(
+                              onTap: (){},
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white.withOpacity(0.5),
+                                    borderRadius: BorderRadius.circular(20)
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      const SizedBox(),
+                                      Image.asset(
+                                        "assets/images/diary.png",
+                                        height: 80,
+                                      ),
+                                      const SizedBox(),
+                                      const Text(
+                                        "Diary",
+                                        style: TextStyle(
+                                            fontFamily: "Lemon Milk",
+                                            fontSize: 18,
+                                            color: Colors.black
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                          ),
+                          // AI Bot
+                          InkWell(
+                              onTap: (){},
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white.withOpacity(0.5),
+                                    borderRadius: BorderRadius.circular(20)
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      const SizedBox(),
+                                      Image.asset(
+                                        "assets/images/robot.png",
+                                        height: 80,
+                                      ),
+                                      const SizedBox(),
+                                      const Text(
+                                        "AI Bot",
+                                        style: TextStyle(
+                                            fontFamily: "Lemon Milk",
+                                            fontSize: 18,
+                                            color: Colors.black
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                          ),
+                          // Profile
+                          InkWell(
+                              onTap: (){},
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white.withOpacity(0.5),
+                                    borderRadius: BorderRadius.circular(20)
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      const SizedBox(),
+                                      Image.asset(
+                                        "assets/images/womanProfile.png",
+                                        height: 80,
+                                      ),
+                                      const SizedBox(),
+                                      const Text(
+                                        "Profile",
+                                        style: TextStyle(
+                                            fontFamily: "Lemon Milk",
+                                            fontSize: 18,
+                                            color: Colors.black
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                          ),
 
-                            ),
-                          )
+
                         ],
                       )
                     ],
